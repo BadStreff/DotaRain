1ac1a31e649218f22815bb2f12cc94d2c18ecd3c
BadStreff <adamfurbee2010@gmail.com>
2013-08-27 22:41:11 -0500

    time threshold
    Started working on a function to check the time stamp of the file we are
    creating if it already exist so that we aren't downloading the file
    multiple times per minute

--- SteamWebAPI.cpp
+++ SteamWebAPI.cpp
@@ -1,7 +1,6 @@
 #include "SteamWebAPI.h"
 
 std::string SteamWebAPI::getMatchHistory(std::string numMatches){
-	//https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML&key=A2ABB033484CA4908799677CCC313B83&account_id=31856012
 	std::string url = std::string("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=XML");
 	url.append(std::string("&key=").append(API_KEY));
 	url.append(std::string("&account_id=").append(ACCOUNT_ID));
@@ -29,14 +28,21 @@ TODO: Check if the file is older than 5-10 minutes, if not just return file path
 */
 std::string SteamWebAPI::getSteamXML(std::string URL, std::string filename){
 	std::string path = std::string("C:\\Windows\\Temp\\").append(filename);
-	
-	//checking will go here
 
-	HANDLE file = CreateFile(s2ws(path).c_str(),(GENERIC_READ | GENERIC_WRITE), 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE file = CreateFile(s2ws(path).c_str(),(GENERIC_READ | GENERIC_WRITE), 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	//checking to see if the file is still relatively new, new meaning ~10 minutes here
+	//TODO: break this into another function
+	_BY_HANDLE_FILE_INFORMATION fileInfo;
+	GetFileInformationByHandle(file, &fileInfo);
+	_SYSTEMTIME fileSysTime;
+	FileTimeToSystemTime((&(fileInfo.ftCreationTime)), &fileSysTime);
+	std::cout << "File Creation Time of " << filename << ": " << fileSysTime.wMinute << std::endl;
+	SYSTEMTIME st;
+	GetSystemTime(&st);
 
 	HINTERNET hOpen = InternetOpen(L"DotaRain", NULL, NULL, NULL, NULL);
 	HINTERNET hURL = InternetOpenUrl(hOpen, s2ws(URL).c_str(), NULL, NULL, INTERNET_FLAG_RELOAD | INTERNET_FLAG_DONT_CACHE, NULL);
-
 
 	DWORD dwBytesRead =0;
 	DWORD dwBytesWritten = 0;
@@ -45,7 +51,6 @@ std::string SteamWebAPI::getSteamXML(std::string URL, std::string filename){
 		ZeroMemory(lpBuffer, 2000);
 		InternetReadFile(hURL, (LPVOID)lpBuffer, 2000, &dwBytesRead);
 		WriteFile(file, (LPVOID)lpBuffer, dwBytesRead, &dwBytesWritten, NULL);
-		//std::cout << lpBuffer << std::endl;
 		delete[] lpBuffer;
 		lpBuffer = NULL;
 	} while (dwBytesRead);
