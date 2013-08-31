f2218d652ef39c4f5a209eefaf51908de45c5ed1
BadStreff <adamfurbee2010@gmail.com>
2013-08-31 16:44:14 -0500

    added player details
    match details now contains a vector of the 10 players

--- MatchDetails.cpp
+++ MatchDetails.cpp
@@ -4,6 +4,9 @@ MatchDetails::MatchDetails(std::string XMLPath){
 	tinyxml2::XMLDocument match_details;
 	match_details.LoadFile(XMLPath.c_str());
 	getDetails(match_details.FirstChildElement("result")->FirstChildElement("radiant_win"));
+
+	tinyxml2::XMLNode *players = match_details.FirstChildElement("result")->FirstChildElement("players")->FirstChildElement("player");
+	getPlayerDetails(players);
 }
 
 
@@ -13,5 +16,14 @@ void MatchDetails::getDetails(tinyxml2::XMLNode *n){
 	else{
 		details[n->ToElement()->Name()] += n->ToElement()->GetText();
 		return getDetails(n->NextSibling());
+	}
+}
+
+void MatchDetails::getPlayerDetails(tinyxml2::XMLNode *n){
+	if (n==0)
+		return;
+	else{
+		pDetails.push_back(PlayerDetails(n));
+		return getPlayerDetails(n->NextSibling());
 	}
 }
--- MatchDetails.h
+++ MatchDetails.h
@@ -9,12 +9,16 @@
 
 #include "tinyxml2\tinyxml2.h"
 #include "SteamWebAPI.h"
+#include "PlayerDetails.h"
 
 class MatchDetails{
 public:
 	MatchDetails(std::string XMLPath);
+	std::string getDetail(std::string s){return details[s];}
+	PlayerDetails getPlayer(int i){return pDetails[i];}
 private:
-	//std::vector< std::map<std::string,std::string> >
 	std::map<std::string,std::string> details;
+	std::vector<PlayerDetails> pDetails;
 	void getDetails(tinyxml2::XMLNode *n);
+	void getPlayerDetails(tinyxml2::XMLNode *n);
 };
